2f7c01743546047de870f0503be42da48fa6af70
Emil L <emillarsson99@gmail.com>
2026-05-31 20:21:32 +0200

    Created SkillsAppService and DI

---
+++ SkillsService.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SkillsService.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<SkillsAppService>();
+        return services;
+    }
+}
---
+++ SkillsService.Application/Services/SkillsAppService.cs
@@ -0,0 +1,44 @@
+﻿using SkillsService.Domain.Entities;
+using SkillsService.Domain.Repositories;
+using SkillsService.Domain.ValueObjects;
+
+namespace SkillsService.Application.Services;
+
+public class SkillsAppService
+{
+    private readonly ISkillCatalogRepository _catalogRepository;
+    private readonly IUserSkillRepository _userSkillRepository;
+
+    public SkillsAppService(ISkillCatalogRepository catalogRepository, IUserSkillRepository userSkillRepository)
+    {
+        _catalogRepository = catalogRepository;
+        _userSkillRepository = userSkillRepository;
+    }
+
+    public async Task<IEnumerable<SkillCatalog>> GetCatalogAsync(CancellationToken ct)
+    {
+        return await _catalogRepository.GetAllAsync(ct);
+    }
+
+    public async Task<IEnumerable<UserSkill>>GetUserSkillsAsync(OwnerId ownerId, CancellationToken ct)
+    {
+        return await _userSkillRepository.GetByOwnerIdAsync(ownerId, ct);
+    }
+
+    public async Task<UserSkill> AddUserSkillAsync(OwnerId ownerId, Guid skillCatalogId, CancellationToken ct)
+    {
+        var userSkill = UserSkill.Create(ownerId, skillCatalogId);
+        await _userSkillRepository.AddAsync(userSkill, ct);
+        return userSkill;
+    }
+
+    public async Task DeleteUserSkillAsync(Guid id, CancellationToken ct)
+    {
+        var userSkill = await _userSkillRepository.GetByIdAsync(id, ct);
+
+        if (userSkill is null)
+            return;
+
+        await _userSkillRepository.DeleteAsync(userSkill, ct);
+    }
+}
