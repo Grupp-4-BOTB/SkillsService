ecfb36b8cd4c0de7ff7b544e429441309a21b8df
Emil L <emillarsson99@gmail.com>
2026-05-31 19:16:14 +0200

    Created OwnerId valueObject

---
+++ SkillsService.Domain/ValueObjects/OwnerId.cs
@@ -0,0 +1,15 @@
+﻿namespace SkillsService.Domain.ValueObjects;
+
+public record OwnerId
+{
+    public string Value { get; }
+    public OwnerId(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new DomainException("OwnerId cannot be null or empty.");
+
+        Value = value.Trim();
+    }
+
+    public override string ToString() => Value;
+}
