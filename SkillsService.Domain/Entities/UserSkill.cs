df8fa9eeb46fc987bd041cccefbb73931d9dd4b1
Emil L <emillarsson99@gmail.com>
2026-05-31 19:28:50 +0200

    Created ISkillCatalogRepository Interface  and UserSkill Entity

--- SkillsService.Domain/Entities/UserSkill.cs
+++ SkillsService.Domain/Entities/UserSkill.cs
@@ -1,10 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SkillsService.Domain.ValueObjects;
 
-namespace SkillsService.Domain.Entities
+namespace SkillsService.Domain.Entities;
+
+public class UserSkill
 {
-    internal class UserSkill
+    public Guid Id { get; private set; }
+    public OwnerId OwnerId { get; private set; }
+    public Guid SkillCatalogId { get; private set; }
+    public SkillCatalog SkillCatalog { get; private set; } = null!;
+    public DateTime CreatedAt {  get; private set; }
+
+    private UserSkill()
     {
+    }
+
+    public static UserSkill Create(OwnerId ownerId, Guid skillCatalogId)
+    {
+        return new UserSkill
+        {
+            Id = Guid.NewGuid(),
+            OwnerId = ownerId,
+            SkillCatalogId = skillCatalogId,
+            CreatedAt = DateTime.UtcNow
+        };
     }
 }
