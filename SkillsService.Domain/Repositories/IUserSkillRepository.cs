9b1e37f12ce7a5006c58d3c0acaeab5c8259a5fe
Emil L <emillarsson99@gmail.com>
2026-05-31 19:31:51 +0200

    Created IUserSkillRepository Interface

---
+++ SkillsService.Domain/Repositories/IUserSkillRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkillsService.Domain.Repositories
+{
+    internal interface IUserSkillRepository
+    {
+    }
+}
