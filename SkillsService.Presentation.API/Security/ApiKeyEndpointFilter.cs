b8b4fcf8c3474718d36a085ce568f5c8ac0286fe
Emil L <emillarsson99@gmail.com>
2026-06-04 21:32:48 +0200

    Created api key and implemented in program.cs

---
+++ SkillsService.Presentation.API/Security/ApiKeyEndpointFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace SkillsService.Presentation.API.Security;
+
+public sealed class ApiKeyEndpointFilter(IOptions<ApiKeyOption> options) : IEndpointFilter
+{
+    private readonly ApiKeyOption _options = options.Value;
+    public ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        if(string.IsNullOrWhiteSpace(_options.Value))
+            return ValueTask.FromResult<object?>(Results.Unauthorized());
+
+        if (!context.HttpContext.Request.Headers.TryGetValue(_options.HeaderName, out var providedApiKey))
+            return ValueTask.FromResult<object?>(Results.Unauthorized());
+
+        if (!string.Equals(providedApiKey, _options.Value, StringComparison.Ordinal))
+            return ValueTask.FromResult<object?>(Results.Unauthorized());
+
+        return next(context);
+    }
+}
